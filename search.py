f16efdcca2f98574da16e8f9fc5c43dd2d7910e9
lierluis <lierluis1@gmail.com>
2017-06-11 17:32:40 -0700

    Combined binary and linear search into 1 file

---
+++ search.py
@@ -0,0 +1,40 @@
+def linear_search(input_list, value):
+    """Search for a value in a list using linear/sequential search.
+
+    Return index of value if found; -1 otherwise
+    Time complexity: O(n)
+    Space complexity: O(1)
+    """
+    for i in range(len(input_list)-1):
+        if input_list[i] == value:
+            return i
+    return -1
+
+
+def binary_search(input_list, value):
+    """Search for a value in a list using binary search.
+
+    Return index of value if found; -1 otherwise
+    Time complexity: O(logn)
+    Space complexity: O(1)
+    """
+    low = 0
+    high = len(input_list)-1
+    while low <= high:
+        mid = (low+high)/2
+        if input_list[mid] == value:
+            return mid
+        elif input_list[mid] < value:
+            low = mid + 1
+        elif input_list[mid] > value:
+            high = mid - 1
+    return -1
+
+
+test_list = [1,3,9,11,15,19,29]
+test_value1 = 25
+test_value2 = 15
+print linear_search("Linear search:", test_list, test_value1)
+print linear_search("Linear search:", test_list, test_value2)
+print binary_search("Binary search:", test_list, test_value1)
+print binary_search("Binary search:", test_list, test_value2)
