710e091ce2316b6f72df199ddb5293ef0d44b9f0
lierluis <lierluis1@gmail.com>
2017-10-21 03:12:13 -0700

    Update tests for queue.py

--- python/queue.py
+++ python/queue.py
@@ -15,27 +15,21 @@ class Queue:
         return self.storage.pop(0)
 
 
-# Setup
+# Tests
 q = Queue(1)
 q.enqueue(2)
 q.enqueue(3)
 
 # Test peek
-# Should be 1
-print q.peek()
+print(q.peek()) # 1
 
 # Test dequeue
-# Should be 1
-print q.dequeue()
+print(q.dequeue()) # 1
 
 # Test enqueue
 q.enqueue(4)
-# Should be 2
-print q.dequeue()
-# Should be 3
-print q.dequeue()
-# Should be 4
-print q.dequeue()
+print(q.dequeue()) # 2
+print(q.dequeue()) # 3
+print(q.dequeue()) # 4
 q.enqueue(5)
-# Should be 5
-print q.peek()
+print(q.peek()) # 5
