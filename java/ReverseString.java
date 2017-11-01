c3248972121f0551fdca022e0a7e393c78fcd565
lierluis <lierluis1@gmail.com>
2017-10-31 20:55:51 -0700

    Add string reversal program

---
+++ java/ReverseString.java
@@ -0,0 +1,31 @@
+public class ReverseString {
+
+    /**
+     * This function reverses a string literal.
+     *
+     * @param str the string literal
+     * @return    the string str reversed
+     */
+    public static String reverseString(String str) {
+        String inputString = str;
+        String outputString = null;
+
+        StringBuilder sb = new StringBuilder();
+
+        if (inputString != null)
+        {
+            for (int i = inputString.length()-1; i >= 0; i--) {
+                sb.append(inputString.charAt(i));
+            }
+            outputString = sb.toString();
+        }
+        return outputString;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("abcd"));
+        System.out.println(reverseString("a"));
+        System.out.println(reverseString(""));
+        System.out.println(reverseString(null));
+    }
+}
