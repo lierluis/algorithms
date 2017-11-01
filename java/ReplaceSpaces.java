36946caf84156d0f0552e729afde1c6ce352a78b
lierluis <lierluis1@gmail.com>
2017-11-01 14:23:35 -0700

    Add replace spaces problem

---
+++ java/ReplaceSpaces.java
@@ -0,0 +1,35 @@
+public class ReplaceSpaces {
+
+    /**
+     * This method replaces all spaces in a string with a replacement string.
+     * <p>
+     * Time complexity: O(n) | Space complexity: O(n)
+     *
+     * @param a the string to iterate over
+     * @param b the replacement string for the spaces in string a
+     * @return  the string a with spaces replaced by string b
+     */
+    public static String replace(String a, String b) {
+        if (b == null || a == null) {
+            return a;
+        }
+        StringBuilder sb = new StringBuilder(a.length());
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            if (c == ' ') {
+                sb.append(b);
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(replace("This is a test", "ABC"));
+        System.out.println(replace("Replace all spaces", "ABC"));
+        System.out.println(replace("This", "ABC"));
+        System.out.println(replace("", "ABC"));
+        System.out.println(replace(null, "ABC"));
+    }
+}
