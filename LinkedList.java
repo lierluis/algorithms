0f3ce09e405df8e3b16f593fccac9953a488fa8b
lierluis <lierluis1@gmail.com>
2017-11-28 22:00:32 -0800

    Add linked list with add method

---
+++ LinkedList.java
@@ -0,0 +1,55 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+    public LinkedList(Node head) {
+        this.head = head;
+    }
+
+    /**
+     * This method adds a new node to the linked list.
+     * <p>
+     * Time complexity: O(n) | Space complexity: O(1)
+     *
+     * @param newNode the new node to add to the linked list
+     */
+    public void add(Node newNode) {
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.print();
+    }
+
+    public void print() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.add(new Node(1));
+        ll.add(new Node(2));
+        ll.add(new Node(3));
+    }
+}
