9d675bab9647ce1ac0de71fc95b52c7a6b63f296
lierluis <lierluis1@gmail.com>
2017-12-02 01:16:59 -0800

    Make fib method concise + add documentation

--- java/Fibonacci.java
+++ java/Fibonacci.java
@@ -1,13 +1,19 @@
 public class Fibonacci {
 
+    /**
+     * This method recursively finds the nth number in the fibonacci sequence.
+     * <p>
+     * Time complexity: O(n^2) | Space complexity: O(n) memory
+     *
+     * @param n the index in the fibonacci sequence
+     * @return  the nth number in the fibonacci sequence
+     */
     public static int fib(int n) {
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        return fib(n-1) + fib(n-2);
+        return (n < 2) ? n : fib(n-1) + fib(n-2);
     }
 
     public static void main(String[] args) {
+        System.out.println(fib(-1));
         System.out.println(fib(0));
         System.out.println(fib(1));
         System.out.println(fib(10));
