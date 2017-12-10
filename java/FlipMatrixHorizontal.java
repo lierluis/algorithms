13726bf8fbaa37a4167f3b875c312b178a558c89
lierluis <lierluis1@gmail.com>
2017-12-10 13:58:08 -0800

    Add flip matrix horizontal problem

---
+++ java/FlipMatrixHorizontal.java
@@ -0,0 +1,49 @@
+
+public class FlipMatrixHorizontal {
+    // java.util.* has been imported for this problem.
+// You don't need any other imports.
+
+    /**
+     * This method flips a matrix on its horizontal axis.
+     *
+     * <p>Time complexity: O(n^2) | Space complexity: O(1)
+     *
+     * @param matrix the matrix to be flipped
+     */
+    public static void flipHorizontalAxis(int[][] matrix) {
+        for (int row = 0; row < matrix.length / 2; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[matrix.length-1-row][col];
+                matrix[matrix.length-1-row][col] = temp;
+            }
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int col : row) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][][] matrices = {
+            {{0,0,1},{1,0,0}},
+            {{1,0,1},{1,0,1}},
+            {{7,8,9},{4,5,6},{1,2,3}},
+            {{1,0}},
+            {{1}}
+        };
+        for (int[][] matrix : matrices) {
+            System.out.println("Before: ");
+            printMatrix(matrix);
+            flipHorizontalAxis(matrix);
+            System.out.println("After: ");
+            printMatrix(matrix);
+            System.out.println();
+        }
+    }
+}
